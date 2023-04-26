7284be5a001111aad42d54e22f8324e5e10b2872
Nikola Lezaic <lezaicnikola10@gmail.com>
2023-04-26 21:19:08 +0200

    added moment so i can display time and date in left.side component, added styling for 3h weather display

--- src/components/left.side/left.side.component.js
+++ src/components/left.side/left.side.component.js
@@ -1,25 +1,59 @@
 import "./left.side.component.styles.scss";
+import moment from "moment/moment";
 
 const LeftSide = ({ data }) => {
   return (
     <div className="wrapper-left">
       <div className="top-display-date-clock">
-        <p>25 April 2023</p>
-        <p>11:00</p>
+        <p className="date">{moment().format("Do MMMM YYYY")} </p>
+        <div className="border_one"></div>
+        <p className="time">{moment().format("LT")}</p>
       </div>
-      <img
-        alt="weather"
-        className="weather-icon"
-        src={`icons/${data && data.weather[0].icon}.png`}
-      />
-      <p>{data && data.weather[0].description}</p>
+      {data ? (
+        <>
+          <div className="weather-icon-description">
+            <div className="weather-icon">
+              <img alt="weather" src={`icons/${data.weather[0].icon}.png`} />
+            </div>
+            <p className="weather-description">{data.weather[0].description}</p>
+          </div>
+        </>
+      ) : (
+        <p className="Message">Enter the city for weather information.</p>
+      )}
       <div className="bottom-display-wheater-info-line">
+        <div className="border_bottom"></div>
         <div className="by-3h-container">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
+          <div className="by-3h-card">
+            <p className="by-3h-time">9:00</p>
+            <div className="border_two"></div>
+            <div>icon</div>
+            <p className="by-3h-temperature">9°C</p>
+          </div>
+          <div className="by-3h-card">
+            <p className="by-3h-time">12:00</p>
+            <div className="border_two"></div>
+            <div>icon</div>
+            <p className="by-3h-temperature">9°C</p>
+          </div>
+          <div className="by-3h-card">
+            <p className="by-3h-time">15:00</p>
+            <div className="border_two"></div>
+            <div>icon</div>
+            <p className="by-3h-temperature">9°C</p>
+          </div>
+          <div className="by-3h-card">
+            <p className="by-3h-time">18:00</p>
+            <div className="border_two"></div>
+            <div>icon</div>
+            <p className="by-3h-temperature">9°C</p>
+          </div>
+          <div className="by-3h-card">
+            <p className="by-3h-time">21:00</p>
+            <div className="border_two"></div>
+            <div>icon</div>
+            <p className="by-3h-temperature">9°C</p>
+          </div>
         </div>
       </div>
     </div>
