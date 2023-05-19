2b6e16134b310fda430a9660ea0957138ced149d
Nikola Lezaic <lezaicnikola10@gmail.com>
2023-05-20 00:24:20 +0200

    added button for cities in serbia only search, had to adjust media sizes again

--- src/components/right.side/right.side.component.js
+++ src/components/right.side/right.side.component.js
@@ -2,11 +2,27 @@ import "./right.side.component.styles.scss";
 import Search from "../search/search";
 import CurrentWeather from "./current-weather/current-weather";
 import ForecastDays from "./forecast-days/forecast-days";
+import SearchRS from "../search/search.rs";
+import { useState } from "react";
 
 const RightSide = ({ handleOnSearchChange, currentWeather, forecast }) => {
+  const [isRsSearch, setIsRsSearch] = useState(false);
+
+  const handleSearchbarChange = () => {
+    setIsRsSearch(!isRsSearch);
+  };
+
   return (
     <div className="wrapper-right">
-      <Search onSearchChange={handleOnSearchChange} />
+      {isRsSearch ? (
+        <SearchRS onSearchChange={handleOnSearchChange} />
+      ) : (
+        <Search onSearchChange={handleOnSearchChange} />
+      )}
+      <button className="button" onClick={handleSearchbarChange}>
+        {isRsSearch ? "Search for All" : "Search in Serbia"}
+      </button>
+
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <ForecastDays data={forecast} />}
     </div>
---
+++ src/components/search/search.rs.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import "./search.rs.styles.scss";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
+
+const SearchRS = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/cities?limit=10&countryIds=RS&namePrefix=${inputValue}`,
+        geoApiOptions
+      );
+      const response_data = await response.json();
+      const options = response_data.data.map((city) => ({
+        value: `${city.latitude} ${city.longitude}`,
+        label: `${city.name}, ${city.countryCode}`,
+      }));
+      return { options };
+    } catch (error) {
+      console.error(error);
+      return { options: [] };
+    }
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      backgroundColor: "#010d0fc",
+      borderRadius: "8px",
+      border: "1px solid #576c69",
+      width: "100%",
+      boxShadow: state.isFocused ? "0 0 0 2px #576c69" : null,
+      "&:hover": {
+        backgroundColor: "#010d0c",
+      },
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: "#010d0f",
+      color: state.isFocused ? "whitesmoke" : null,
+      color: "whitesmoke",
+      "&:hover": {
+        backgroundColor: "#a7b1b1",
+      },
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: "whitesmoke",
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: "#010d0fc",
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: "whitesmoke",
+    }),
+  };
+  return (
+    <AsyncPaginate
+      placeholder="Search cities in Serbia..."
+      className="search-input"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+      styles={customStyles}
+    />
+  );
+};
+export default SearchRS;
